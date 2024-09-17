b9e4b1dd152fd05160f749b01326401638b99935
unknown <netoelpidio1@gmail.com>
2024-09-17 08:28:43 -0300

    arquivo de crud basico para a ap2

---
+++ ap2web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MyNavbar from "./components/mynavbar/MyNavbar.js";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
+}
+
+export default App;
